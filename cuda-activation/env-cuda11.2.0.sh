f3984fc9982e3973133b018e71a5cdd87cb84257
Kurt Mohler <kurtamohler@gmail.com>
2022-02-09 14:55:42 -0600

    Add error message if CUDA installation not found

--- cuda-activation/env-cuda11.2.0.sh
+++ cuda-activation/env-cuda11.2.0.sh
@@ -1,4 +1,11 @@
 export CUDA_HOME=/usr/local/cuda-11.2.0
+
+if [ ! -d $CUDA_HOME ]; then
+  echo "Error: CUDA directory does not exist: $CUDA_HOME"
+  echo "       Please install it"
+  exit 1
+fi
+
 export PATH=$CUDA_HOME/bin:$PATH
 export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
 export LDFLAGS="$LDFLAGS -Wl,-rpath,$CUDA_HOME/lib64 -Wl,-rpath-link,$CUDA_HOME/lib64 -L$CUDA_HOME/lib64"
